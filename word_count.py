3429d454da1e3a26cbe067d85725d04f024d325c
Akhil Sankar <akhil.sankar@gmail.com>
2016-07-23 18:47:30 -0400

    alphabetized

--- word_count.py
+++ word_count.py
@@ -1,28 +1,6 @@
-# """Count words."""
-#
-# def count_words(s, n):
-#     """Return the n most frequently occuring words in s."""
-#
-#     # TODO: Count the number of occurences of each word in s
-#
-#     # TODO: Sort the occurences in descending order (alphabetically in case of ties)
-#
-#     # TODO: Return the top n words as a list of tuples (<word>, <count>)
-#     return top_n
-#
-#
-# def test_run():
-#     """Test count_words() with some inputs."""
-#     print count_words("cat bat mat cat bat cat", 3)
-#     print count_words("betty bought a bit of butter but the butter was bitter", 3)
-#
-#
-# if __name__ == '__main__':
-#     test_run()
+"""Count words."""
 
-
-def word_count(s, n=None):
-
+def count_words(s, n):
     words = s.split(" ")
     word_count_array = []
 
@@ -30,16 +8,34 @@ def word_count(s, n=None):
         if not word in map(lambda word_hash: word_hash["word"], word_count_array):
             word_count_array.append({"word": word, "count": words.count(word)})
 
-    #key takes in one value and returns one value, before sort
     descending_word_count = sorted(
                                     word_count_array,
                                     key=lambda word_hash: word_hash["count"],
                                     reverse=True)
 
-    # for i in xrange(0, n):
+    max_count = descending_word_count[0]["count"]
+
+    all_sorted_hashes = []
+    for count in reversed(range(1, max_count+1)):
+        count_group = []
+        for word_hash in descending_word_count:
+            if word_hash["count"] == count:
+                count_group.append(word_hash)
+        alphabetized_group = sorted(count_group)
+        for word_hash in alphabetized_group:
+            converted_tuple = (word_hash["word"], word_hash["count"])
+            all_sorted_hashes.append(converted_tuple)
+
+    top_n = all_sorted_hashes[:n]
+
+    return top_n
 
 
+def test_run():
+    """Test count_words() with some inputs."""
+    print count_words("cat bat mat cat bat cat", 3)
+    print count_words("betty bought a bit of butter but the butter was bitter", 3)
 
-    print descending_word_count
 
-word_count("betty bought a bit of butter but the bit of butter was bitter")
+if __name__ == '__main__':
+    test_run()
